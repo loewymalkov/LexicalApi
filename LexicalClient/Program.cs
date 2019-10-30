d730eebfe02ac19aeffbe615b64fee73c385f6c5
Student <example@example.com>
2019-10-30 15:42:40 -0700

    Add startup file to lexicalclient

--- LexicalClient/Program.cs
+++ LexicalClient/Program.cs
@@ -5,17 +5,31 @@ using LexicalClient.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RestSharp;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ApiTest {
-  class Program {
-    static void Main () {
-      var client = new RestClient ("http://localhost:5000/api/words");
-      var request = new RestRequest ("resource/{id}", Method.POST);
-      var word = new {Name = "dude", Rating=5, RatingCount = 0};
-      request.AddJsonBody(word);
-      var response = client.Get(request);
+namespace LexicalClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // var client = new RestClient ("http://localhost:5000/api/words");
+            // var request = new RestRequest ("resource/{id}", Method.POST);
+            // var word = new {Name = "dude", Rating=5, RatingCount = 0};
+            // request.AddJsonBody(word);
+            // var response = client.Get(request);
 
-      Console.WriteLine(response.ToString());
+            // Console.WriteLine(response.ToString());
+
+            CreateWebHostBuilder(args).Build().Run();
+
+
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                  WebHost.CreateDefaultBuilder(args)
+                      .UseStartup<Startup>();
     }
-  }
-} 
+}
+
